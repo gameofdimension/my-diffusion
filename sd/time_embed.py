7bf608fcf0df447243d1d53c7e661aa7fd6d9682
felixdae <felixdae@gmail.com>
2024-04-17 03:51:09 +0000

    time embeb seems ok

---
+++ sd/time_embed.py
@@ -0,0 +1,172 @@
+from diffusers.models.embeddings import Timesteps as GoldTimesteps
+from diffusers.models.embeddings import TimestepEmbedding as GoldTransfer
+import math
+
+import torch
+from torch import nn
+
+
+class TimestepEmbedding(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        time_embed_dim: int,
+    ):
+        super().__init__()
+        act_fn: str = "silu"
+        # out_dim: int = None,
+        # post_act_fn: Optional[str] = None,
+        # cond_proj_dim=None,
+        sample_proj_bias = True
+        # linear_cls = nn.Linear if USE_PEFT_BACKEND else LoRACompatibleLinear
+
+        self.linear_1 = nn.Linear(
+            in_channels, time_embed_dim, sample_proj_bias)
+
+        # if cond_proj_dim is not None:
+        #     self.cond_proj = nn.Linear(cond_proj_dim, in_channels, bias=False)
+        # else:
+        #     self.cond_proj = None
+
+        assert act_fn == "silu"
+        self.act = nn.SiLU()
+
+        # if out_dim is not None:
+        #     time_embed_dim_out = out_dim
+        # else:
+        #     time_embed_dim_out = time_embed_dim
+        time_embed_dim_out = time_embed_dim
+        self.linear_2 = nn.Linear(
+            time_embed_dim, time_embed_dim_out, sample_proj_bias)
+
+        # if post_act_fn is None:
+        #     self.post_act = None
+        # else:
+        #     self.post_act = get_activation(post_act_fn)
+
+    def forward(self, sample, condition=None):
+        # if condition is not None:
+        #     sample = sample + self.cond_proj(condition)
+        sample = self.linear_1(sample)
+
+        # if self.act is not None:
+        sample = self.act(sample)
+
+        sample = self.linear_2(sample)
+
+        # if self.post_act is not None:
+        #     sample = self.post_act(sample)
+        return sample
+
+
+def get_timestep_embedding(
+    timesteps: torch.Tensor,
+    embedding_dim: int,
+    flip_sin_to_cos: bool = False,
+    downscale_freq_shift: float = 1,
+    scale: float = 1,
+    max_period: int = 10000,
+):
+    """
+    This matches the implementation in Denoising Diffusion Probabilistic
+    Models: Create sinusoidal timestep embeddings.
+
+    :param timesteps: a 1-D Tensor of N indices, one per batch element.
+                      These may be fractional.
+    :param embedding_dim: the dimension of the output.
+    :param max_period: controls the minimum frequency of the embeddings.
+    :return: an [N x dim] Tensor of positional embeddings.
+    """
+    assert len(timesteps.shape) == 1, "Timesteps should be a 1d-array"
+
+    half_dim = embedding_dim // 2
+    exponent = -math.log(max_period) * torch.arange(
+        start=0, end=half_dim, dtype=torch.float32, device=timesteps.device
+    )
+    exponent = exponent / (half_dim - downscale_freq_shift)
+
+    emb = torch.exp(exponent)
+    emb = timesteps[:, None].float() * emb[None, :]
+
+    # scale embeddings
+    emb = scale * emb
+
+    # concat sine and cosine embeddings
+    emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=-1)
+
+    # flip sine and cosine embeddings
+    if flip_sin_to_cos:
+        emb = torch.cat([emb[:, half_dim:], emb[:, :half_dim]], dim=-1)
+
+    # zero pad
+    if embedding_dim % 2 == 1:
+        emb = torch.nn.functional.pad(emb, (0, 1, 0, 0))
+    return emb
+
+
+class Timesteps(torch.nn.Module):
+    def __init__(
+            self, num_channels: int, flip_sin_to_cos: bool,
+            downscale_freq_shift: float):
+        super().__init__()
+        self.num_channels = num_channels
+        self.flip_sin_to_cos = flip_sin_to_cos
+        self.downscale_freq_shift = downscale_freq_shift
+
+    def forward(self, timesteps):
+        t_emb = get_timestep_embedding(
+            timesteps,
+            self.num_channels,
+            flip_sin_to_cos=self.flip_sin_to_cos,
+            downscale_freq_shift=self.downscale_freq_shift,
+        )
+        return t_emb
+
+
+def test_embedding():
+    torch.manual_seed(0)
+
+    timesteps = torch.randint(0, 1000, (10,))
+    mod_timesteps = Timesteps(
+        320, flip_sin_to_cos=True, downscale_freq_shift=0)
+    embeddings = mod_timesteps(timesteps)
+
+    gold_mod = GoldTimesteps(
+        320, flip_sin_to_cos=True, downscale_freq_shift=0)
+    gold_embeddings = gold_mod(timesteps)
+
+    print((embeddings-gold_embeddings).abs().max())
+
+
+def test_transform_embedding():
+    in_channels = 320
+    out_channels = 320*4
+
+    weight1 = torch.randn(out_channels, in_channels)
+    bias1 = torch.randn(out_channels)
+
+    weight2 = torch.randn(out_channels, out_channels)
+    bias2 = torch.randn(out_channels)
+
+    mod = TimestepEmbedding(in_channels, out_channels)
+    mod.linear_1.weight.data.copy_(weight1)
+    mod.linear_1.bias.data.copy_(bias1)
+    mod.linear_2.weight.data.copy_(weight2)
+    mod.linear_2.bias.data.copy_(bias2)
+
+    gold_mod = GoldTransfer(in_channels, out_channels)
+    gold_mod.linear_1.weight.data.copy_(weight1)
+    gold_mod.linear_1.bias.data.copy_(bias1)
+    gold_mod.linear_2.weight.data.copy_(weight2)
+    gold_mod.linear_2.bias.data.copy_(bias2)
+
+    data = torch.randn(10, in_channels)
+    out = mod(data)
+    gold = gold_mod(data)
+
+    print((out-gold).abs().max())
+
+
+if __name__ == "__main__":
+    # test_embedding()
+    test_transform_embedding()
--- sd/unet15.py
+++ sd/unet15.py
@@ -1,9 +1,76 @@
+import math
+# import json
+from typing import Tuple
+
+import diffusers
+import torch
+# from deepspeed.accelerator import get_accelerator
+# from deepspeed.profiling.flops_profiler import get_model_profile
 from diffusers import UNet2DConditionModel
 from transformers import PretrainedConfig
 
-import json
-import diffusers
-import torch
+
+class CrossAttnDownBlock2D(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class DownBlock2D(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class UpBlock2D(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class CrossAttnUpBlock2D(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+
+class CondtionalUNet(torch.nn.Module):
+    in_channels: int = 4
+    out_channels: int = 4
+    block_out_channels: Tuple[int] = (320, 640, 1280, 1280)
+    conv_in_kernel: int = 3
+    conv_out_kernel: int = 3
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        conv_in_padding = (self.conv_in_kernel - 1) // 2
+        self.conv_in = torch.nn.Conv2d(
+            self.in_channels, self.block_out_channels[0],
+            kernel_size=self.conv_in_kernel, padding=conv_in_padding
+        )
+
+        timestep_input_dim = self.block_out_channels[0]
+        time_embed_dim = timestep_input_dim * 4
+
+        self.time_proj = Timesteps(
+            timestep_input_dim, flip_sin_to_cos, freq_shift)
+
+        self.down_blocks = torch.nn.ModuleList()
+        self.middle_block = torch.nn.Module()
+        self.up_blocks = torch.nn.ModuleList()
+
+    def forward(self):
+        pass
 
 
 def main():
@@ -12,10 +79,11 @@ def main():
 
 if __name__ == "__main__":
     # main()
-    checkpoint = 'runwayml/stable-diffusion-v1-5'
+    # checkpoint = 'runwayml/stable-diffusion-v1-5'
+    checkpoint = 'stabilityai/stable-diffusion-2-1'
 
     config = PretrainedConfig.get_config_dict(checkpoint, subfolder='unet')[0]
-    print(json.dumps(config, indent=4))
+    # print(json.dumps(config, indent=4))
     # cfg = AutoConfig.from_pretrained(checkpoint, subfolder='unet')
     # print(cfg)
     print(diffusers.utils.constants.USE_PEFT_BACKEND)
@@ -25,18 +93,33 @@ if __name__ == "__main__":
         checkpoint, subfolder="unet",
     ).to(device)
 
+    # for m in unet.modules():
+    #     print(m)
+
     bsz = 4
     latents = torch.randn(bsz, 4, 64, 64, device=device)
     timestep = torch.randint(0, 1000, (bsz, ), device=device)
-    condition = torch.randn(bsz, 77, 768, device=device)
+    # condition = torch.randn(bsz, 77, 768, device=device)
+    condition = torch.randn(bsz, 77, 1024, device=device)
 
-    out = unet(latents, timestep, condition, return_dict=False)
-    print(out[0].size())
+    # with get_accelerator().device(0):
+    #     flops, macs, params = get_model_profile(
+    #         unet,
+    #         args=[latents, timestep, condition],
+    #         kwargs={"return_dict": False},
+    #         print_profile=True,
+    #         detailed=True,
+    #         output_file='unet21_flops.txt',
+    #     )
+
+    #     out = unet(latents, timestep, condition, return_dict=False)
+    # print(out[0].size())
 
     # att = unet.attn_processors
     # for k in att:
     #     print(k, att[k].__class__.__name__)
 
-    # # print(unet)
-    # for param in unet.named_parameters():
-    #     print(param[0], param[1].size())
+    # print(unet)
+    for param in unet.named_parameters():
+        print(param[0], param[1].size())
+        break
