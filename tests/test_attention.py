3207e18612cb5e8767fa337cc4a0be1ad65e8d11
felixdae <felixdae@gmail.com>
2024-04-19 07:43:21 +0000

    refactor tests

---
+++ tests/test_attention.py
@@ -0,0 +1,58 @@
+import unittest
+
+import torch
+from diffusers import UNet2DConditionModel
+from diffusers.models.attention import Attention as GoldAttention
+
+from sd.transformer import Attention
+
+
+class ModelTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        checkpoint = 'stabilityai/stable-diffusion-2-1'
+        cls.unet = UNet2DConditionModel.from_pretrained(
+            checkpoint, subfolder="unet",
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_attention(self):
+        for mod in self.unet.named_modules():
+            if isinstance(mod[1], GoldAttention):
+                gold_attn = mod[1]
+                query_dim = gold_attn.query_dim
+                cross_attention_dim = None
+                if mod[0].endswith('attn1'):
+                    attn = Attention(
+                        query_dim=query_dim,
+                        cross_attention_dim=None,
+                        heads=gold_attn.heads,
+                        dim_head=64,
+                    )
+                else:
+                    cross_attention_dim = gold_attn.cross_attention_dim
+                    attn = Attention(
+                        query_dim=query_dim,
+                        cross_attention_dim=cross_attention_dim,
+                        heads=gold_attn.heads,
+                        dim_head=64,
+                    )
+
+                attn.load_state_dict(gold_attn.state_dict())
+                data = torch.randn(40, 120, query_dim)
+
+                if cross_attention_dim is None:
+                    output = attn(data)
+                    gold = gold_attn(data)
+                else:
+                    cross_data = torch.randn(40, 210, cross_attention_dim)
+                    output = attn(data, cross_data)
+                    gold = gold_attn(data, cross_data)
+
+                delta = (gold - output).abs().max().item()
+                print(mod[0], delta)
+                assert delta < 1e-6
---
+++ tests/test_feedforward.py
@@ -0,0 +1,40 @@
+import unittest
+
+import torch
+from diffusers import UNet2DConditionModel
+from diffusers.models.attention import FeedForward as GoldFeedForward
+
+from sd.transformer import FeedForward
+
+
+class ModelTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        checkpoint = 'stabilityai/stable-diffusion-2-1'
+        cls.unet = UNet2DConditionModel.from_pretrained(
+            checkpoint, subfolder="unet",
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_ff(self):
+        for mod in self.unet.named_modules():
+            if isinstance(mod[1], GoldFeedForward):
+                gold_ff = mod[1]
+                dim = gold_ff.net[0].proj.weight.size(1)
+                ff = FeedForward(
+                    dim=dim,
+                )
+                ff.load_state_dict(gold_ff.state_dict())
+
+                data = torch.randn(120, dim)
+
+                output = ff(data)
+                gold = gold_ff(data)
+
+                delta = (gold - output).abs().max().item()
+                print(mod[0], delta)
+                assert delta < 1e-6
